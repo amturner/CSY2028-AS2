011457bf598da85b6e9810c093ab04d479bc5bb0
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-03-31 22:07:58 +0100

    Renamed loginform.html.php to login.html.php

--- templates/admin/login.html.php
+++ templates/admin/login.html.php
@@ -1,5 +1,6 @@
 <main class="home">
     <h2>Log in</h2>
+    <?=(isset($error)) ? '<p>' . $error . '</p>' : '';?>
     <form action="" method="post">
         <label for="username">Username:</label> <input type="text" name="login[username]" id="username">
         <label for="password">Password:</label> <input type="password" name="login[password]" id="passsword">
