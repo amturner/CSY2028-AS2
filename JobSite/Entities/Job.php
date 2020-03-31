25833e2b1ca4ba62a6db96c1d37cc36f4376f1ac
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-03-31 22:05:52 +0100

    Updated Entity classes

--- JobSite/Entities/Job.php
+++ JobSite/Entities/Job.php
@@ -1,6 +1,8 @@
 <?php
 namespace JobSite\Entities;
 class Job {
+    private $applicantsTable;
+
     public $id;
     public $title;
     public $description;
@@ -8,5 +10,13 @@ class Job {
     public $closingDate;
     public $categoryId;
     public $location;
+
+    public function __construct(\CSY2028\DatabaseTable $applicantsTable) {
+        $this->applicantsTable = $applicantsTable;
+    }
+
+    public function getApplicantsCount() {
+        return count($this->applicantsTable->retrieveRecord('jobId', $this->id));
+    }
 }
 ?>
--- JobSite/Entities/User.php
+++ JobSite/Entities/User.php
@@ -1,10 +1,11 @@
 <?php
 namespace JobSite\Entities;
 class User {
-    public $user_id;
+    public $id;
     public $username;
     public $email;
     public $password;
     public $active;
+    public $administrator;
 }
 ?>
