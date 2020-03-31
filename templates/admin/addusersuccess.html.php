c54dcfb3263d5ee7892de8dcbc9f7330300648be
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-03-31 22:08:29 +0100

    Renamed registersuccess.html.php to addusersuccess.html.php

--- templates/admin/addusersuccess.html.php
+++ templates/admin/addusersuccess.html.php
@@ -2,9 +2,9 @@
     <?php require 'adminnav.html.php'; ?>
 
     <section class="right">
-        <h2>Account created</h2>
-        <p>The account '<?=$username;?>' was created successfully!</p>
-        <p><a href="/admin/adduser">Create another account</a></p>
+        <h2>User created</h2>
+        <p>The user <b><?=$username;?></b> was created successfully!</p>
+        <p><a href="/admin/adduser">Create another user</a></p>
         <p><a href="/admin/users">View all users</a></p>
     </section>
 </main>
