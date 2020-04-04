9af2d0bf974c298055f421e30c5d584788d9617b
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-04-04 22:48:27 +0100

    Start setting up unit tests

---
+++ tests/ControllersTest.php
@@ -0,0 +1,12 @@
+<?php
+require '../JobSite/Controllers/JobSiteController.php';
+require '../JobSite/Controllers/AdminController.php';
+require '../JobSite/Controllers/JobController.php';
+require '../JobSite/Controllers/UserController.php';
+require '../JobSite/Controllers/CategoryController.php';
+require '../JobSite/Controllers/EnquiryController.php';
+
+class ControllersTest extends \PHPUnit\Framework\TestCase {
+
+}
+?>
