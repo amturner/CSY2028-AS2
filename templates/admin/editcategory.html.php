d6caeebf5494ee1de7660d234c4c58d60f0a11e8
Alexander Turner <cloud-alex@hotmail.co.uk>
2020-06-26 11:40:24 +0100

    Fixed an issue where errors would not be displayed on the edit category page.

--- templates/admin/editcategory.html.php
+++ templates/admin/editcategory.html.php
@@ -1,6 +1,15 @@
 <?php require 'userpanel.html.php'; ?>
 <h2><?=(isset($_GET['id'])) ? 'Edit Category' : 'Add Category' ;?></h2>
-
+<?php if (isset($errors) && count($errors) > 0): ?>
+    <div class="errors">
+        <p>The category could not be <?=(isset($_GET['id'])) ? 'updated' : 'added';?>:</p>
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?=$error;?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
 <form action="" method="POST">
     <p class="required">Required: </p>
     <input type="hidden" name="category[id]" value="<?=(isset($_GET['id'])) ? $category->id : '';?>" />
